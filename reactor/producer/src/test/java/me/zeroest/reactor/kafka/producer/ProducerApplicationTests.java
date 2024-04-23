ac4685e0e9f27f9fdb6085f7b04d51c45c4e0f7b
zeroest <zeroest.me@gmail.com>
2024-04-24 00:16:37 +0900

    Reactor kafka init

---
+++ reactor/producer/src/test/java/me/zeroest/reactor/kafka/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeroest.reactor.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
